f830353bdd1d29b672e448377c789a2735fb6c47
guibrbs <200037994@aluno.unb.br>
2022-04-14 20:51:26 -0300

    :rocket:Setting up bottom area

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/navbar/Navbar'
 import ShowCase from './components/showcase/Showcase'
+import BottomArea from './components/bottom/BottomArea'
 
 function App() {
   return (
     <div className='big-wrapper'>
       <NavBar />
       <ShowCase />
+      <BottomArea />
     </div>
   )
 }
---
+++ src/components/bottom/BottomArea.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BottomArea(){
+    return(
+        <div className="bottom-area">
+            <div className="container">
+                <button className="toggle-btn">
+                    <i className="fa-regular fa-moon"></i>
+                    <i className="fa-regular fa-sun"></i>
+                </button>
+            </div>
+        </div>
+    )
+}
