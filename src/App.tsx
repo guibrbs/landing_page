c88047c85c82a7bc278020848f5af88915a1cc65
guibrbs <200037994@aluno.unb.br>
2022-04-14 00:12:47 -0300

    :rocket:Setting up with images and colors

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './components/navbar/navbar'
+
+function App() {
+  return (
+    <div className='big-wrapper'>
+      <NavBar>
+
+      </NavBar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo  from '../../assets/images/logo.png'
+import './navbar.css'
+
+export default (props :any) => {
+    return(
+        <header>
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt="Logo" />
+                    <h3>Danvo</h3>
+                </div>
+                <div className="links">
+                    <ul>
+                        <li><a href="">Features</a></li>
+                        <li><a href="">Pricing</a></li>
+                        <li><a href="">Testimonials</a></li>
+                        <li><a href="" className="btn">Sign up</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
