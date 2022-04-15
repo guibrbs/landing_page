6d6e8bc43b7b64eccb5b77866456e284b13ec196
guibrbs <200037994@aluno.unb.br>
2022-04-14 21:11:45 -0300

    :rocket:Setting up the shape image

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 import NavBar from './components/navbar/Navbar'
 import ShowCase from './components/showcase/Showcase'
 import BottomArea from './components/bottom/BottomArea'
+import Shape from './assets/images/shape.png'
 
 function App() {
   return (
     <div className='big-wrapper'>
+      <img src={Shape} alt="Shape" className='shape'/>
       <NavBar />
       <ShowCase />
       <BottomArea />
